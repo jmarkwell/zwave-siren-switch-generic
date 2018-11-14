670d75e147848ec39642edddecee74ebe657f4e0
Jordan Markwell <jordan@titaniumnet.com>
2018-11-14 17:29:46 -0600

    Update zwave-siren-switch-generic.groovy

--- devicetypes/jmarkwell/zwave-siren-switch-generic.src/zwave-siren-switch-generic.groovy
+++ devicetypes/jmarkwell/zwave-siren-switch-generic.src/zwave-siren-switch-generic.groovy
@@ -1,10 +1,10 @@
 /**
  *  Z-Wave Siren Switch Generic
- *  Build 2017082901
+ *  Build 2018111401
  *
  *  Adapted from Z-Wave Switch Generic
  *
- *  Copyright 2017 Jordan Markwell
+ *  Copyright 2018 Jordan Markwell
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -17,34 +17,63 @@
  *
  *  ChangeLog:
  *      
+ *      20181114:
+ *          01: Applied updates based on Z-Wave Switch Generic.
+ *
+ *      20170829:
+ *          01: Added code for non-GE model compatibility back in
+ *          02: Updated based on updated Z-Wave Switch Generic to include CRC16 encapsulation
+ *
  *      Earlier:
  *          Creation
- *      
- *      2017082901:
- *          Added code for non-GE model compatibility back in
- *          Updated based on updated Z-Wave Switch Generic to include CRC16 encapsulation
- *      
  */
 metadata {
-    definition(name: "Z-Wave Siren Switch Generic", namespace: "jmarkwell", author: "Jordan Markwell", ocfDeviceType: "oic.d.switch") {
+    definition(name: "Z-Wave Siren Switch Generic", namespace: "jmarkwell", author: "Jordan Markwell", ocfDeviceType: "oic.d.switch", runLocally: true, minHubCoreVersion: '000.019.00012', executeCommandsLocally: true) {
         capability "Actuator"
         capability "Alarm"
         capability "Health Check"
+        capability "Light"
         capability "Polling"
         capability "Refresh"
         capability "Sensor"
         capability "Switch"
         
         fingerprint inClusters: "0x25", deviceJoinName: "Z-Wave Switch"
-        fingerprint mfr:"001D", prod:"1A02", model:"0334", deviceJoinName: "Leviton Appliance Module"
-        fingerprint mfr:"0063", prod:"4F50", model:"3031", deviceJoinName: "GE Plug-in Outdoor Switch"
-        fingerprint mfr:"001D", prod:"1D04", model:"0334", deviceJoinName: "Leviton Outlet"
-        fingerprint mfr:"001D", prod:"1C02", model:"0334", deviceJoinName: "Leviton Switch"
-        fingerprint mfr:"001D", prod:"0301", model:"0334", deviceJoinName: "Leviton 15A Switch"
-        fingerprint mfr:"001D", prod:"0F01", model:"0334", deviceJoinName: "Leviton 5A Incandescent Switch"
-        fingerprint mfr:"001D", prod:"1603", model:"0334", deviceJoinName: "Leviton 15A Split Duplex Receptacle"
-        fingerprint mfr:"011A", prod:"0101", model:"0102", deviceJoinName: "Enerwave On/Off Switch"
-        fingerprint mfr:"011A", prod:"0101", model:"0603", deviceJoinName: "Enerwave Duplex Receptacle"
+        
+        fingerprint mfr: "001A", prod: "5244", deviceJoinName: "Eaton RF Receptacle"
+        fingerprint mfr: "001A", prod: "534C", model: "0000", deviceJoinName: "Eaton RF Master Switch"
+        fingerprint mfr: "001A", prod: "5352", model: "0000", deviceJoinName: "Eaton RF Accessory Switch"
+        fingerprint mfr: "001A", prod: "5354", model: "0003", deviceJoinName: "Eaton RF Appliance Plug-In Module"
+        
+        fingerprint mfr: "0063", prod: "4F50", model: "3031", deviceJoinName: "GE Plug-in Outdoor Switch"
+        fingerprint mfr: "0063", prod: "4F50", model: "3032", deviceJoinName: "GE Plug-in Outdoor Switch"
+        fingerprint mfr: "0063", prod: "5250", model: "3130", deviceJoinName: "GE Plug-in Outdoor Switch"
+        
+        fingerprint mfr: "001D", prod: "0301", model: "0334", deviceJoinName: "Leviton 15A Switch"
+        fingerprint mfr: "001D", prod: "0F01", model: "0334", deviceJoinName: "Leviton 5A Incandescent Switch"
+        fingerprint mfr: "001D", prod: "1603", model: "0334", deviceJoinName: "Leviton 15A Split Duplex Receptacle"
+        fingerprint mfr: "001D", prod: "1A02", model: "0334", deviceJoinName: "Leviton Appliance Module"
+        fingerprint mfr: "001D", prod: "1C02", model: "0334", deviceJoinName: "Leviton Switch"
+        fingerprint mfr: "001D", prod: "1D04", model: "0334", deviceJoinName: "Leviton Outlet"
+        fingerprint mfr: "001D", prod: "3401", model: "0001", deviceJoinName: "Leviton Switch" //Leviton DZ15S
+        
+        fingerprint mfr: "011A", prod: "0101", model: "0102", deviceJoinName: "Enerwave On/Off Switch"
+        fingerprint mfr: "011A", prod: "0101", model: "0603", deviceJoinName: "Enerwave Duplex Receptacle"
+        
+        fingerprint mfr: "0039", prod: "4952", model: "3036", deviceJoinName: "Honeywell Z-Wave In-Wall Smart Switch"
+        fingerprint mfr: "0039", prod: "4952", model: "3037", deviceJoinName: "Honeywell Z-Wave In-Wall Smart Toggle Switch"
+        fingerprint mfr: "0039", prod: "4952", model: "3133", deviceJoinName: "Honeywell Z-Wave In-Wall Tamper Resistant Duplex Receptacle"
+        fingerprint mfr: "0039", prod: "4F50", model: "3032", deviceJoinName: "Honeywell Z-Wave Plug-in Outdoor Smart Switch"
+        fingerprint mfr: "0039", prod: "5052", model: "3033", deviceJoinName: "Honeywell Z-Wave Plug-in Switch (Dual Outlet)"
+        fingerprint mfr: "0039", prod: "5052", model: "3038", deviceJoinName: "Honeywell Z-Wave Plug-in Switch"
+        
+        fingerprint mfr: "014F", prod: "5257", model: "3033", deviceJoinName: "GoControl Wall Relay Switch"
+        fingerprint mfr: "014F", prod: "5753", model: "3535", deviceJoinName: "GoControl Smart In-Wall Switch"
+        
+        //zw:L type:1001 mfr:0307 prod:4447 model:3031 ver:5.06 zwv:4.05 lib:03 cc:5E,86,72,85,59,25,27,73,70,2C,2B,5A,7A role:05 ff:8700 ui:8700
+        fingerprint mfr: "0307", prod: "4447", model: "3031", deviceJoinName: "Satco Plug-In Module"
+        //zw:L type:1001 mfr:0307 prod:4447 model:3033 ver:5.16 zwv:4.34 lib:03 cc:5E,86,72,5A,85,59,73,25,27,70,2C,2B,5B,7A ccOut:5B role:05 ff:8700 ui:8700
+        fingerprint mfr: "0307", prod: "4447", model: "3033", deviceJoinName: "Satco In-Wall Light Switch"
     }
     
     simulator {
@@ -73,19 +102,20 @@ metadata {
 }
 
 def installed(){
-    // device-watch simply pings if no device events received for checkInterval duration of 32min = 2 * 15min + 2min lag time
-    sendEvent(name: "checkInterval", value: 2 * 15 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID])
+    // Device-Watch pings if no events received for checkInterval (32min = 2 * 15min + 2min lag time) duration.
+    sendEvent(name: "checkInterval", value: 2 * 15 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
+    response( refresh() )
 }
 
 def updated(){
-    // device-watch simply pings if no device events received for checkInterval duration of 32min = 2 * 15min + 2min lag time
-    sendEvent(name: "checkInterval", value: 2 * 15 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID])
+    // Device-Watch pings if no events received for checkInterval (32min = 2 * 15min + 2min lag time) duration.
+    sendEvent(name: "checkInterval", value: 2 * 15 * 60 + 2 * 60, displayed: false, data: [protocol: "zwave", hubHardwareId: device.hub.hardwareID, offlinePingable: "1"])
 }
 
 def getCommandClassVersions() {
     [
         0x20: 1,  // basic
-        0x56: 1,  // crc16Encap
+        0x56: 1,  // CRC16Encap
         0x70: 1,  // configuration
     ]
 }
@@ -109,22 +139,15 @@ def parse(String description) {
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicReport cmd) {
-    [name: "alarm", value: cmd.value ? "both" : "off", type: "physical"]
+    [name: "alarm", value: cmd.value ? "both" : "off"]
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.basicv1.BasicSet cmd) {
-    [name: "alarm", value: cmd.value ? "both" : "off", type: "physical"]
+    [name: "alarm", value: cmd.value ? "both" : "off"]
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd) {
-    [name: "alarm", value: cmd.value ? "both" : "off", type: "digital"]
-}
-
-def zwaveEvent(physicalgraph.zwave.commands.configurationv1.ConfigurationReport cmd) {
-    def value = "when off"
-    if (cmd.configurationValue[0] == 1) {value = "when on"}
-    if (cmd.configurationValue[0] == 2) {value = "never"}
-    [name: "indicatorStatus", value: value, display: false]
+    [name: "alarm", value: cmd.value ? "both" : "off"]
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.hailv1.Hail cmd) {
@@ -132,10 +155,10 @@ def zwaveEvent(physicalgraph.zwave.commands.hailv1.Hail cmd) {
 }
 
 def zwaveEvent(physicalgraph.zwave.commands.manufacturerspecificv2.ManufacturerSpecificReport cmd) {
-    log.debug "manufacturerId:   ${cmd.manufacturerId}"
-    log.debug "manufacturerName: ${cmd.manufacturerName}"
-    log.debug "productId:        ${cmd.productId}"
-    log.debug "productTypeId:    ${cmd.productTypeId}"
+    log.debug "manufacturerId:   $cmd.manufacturerId"
+    log.debug "manufacturerName: $cmd.manufacturerName"
+    log.debug "productId:        $cmd.productId"
+    log.debug "productTypeId:    $cmd.productTypeId"
     
     def msr = String.format("%04X-%04X-%04X", cmd.manufacturerId, cmd.productTypeId, cmd.productId)
     updateDataValue("MSR", msr)
@@ -154,7 +177,7 @@ def zwaveEvent(physicalgraph.zwave.commands.crc16encapv1.Crc16Encap cmd) {
     }
 }
 
-// unknown commands
+// This handles unknown commands.
 def zwaveEvent(physicalgraph.zwave.Command cmd) {
     [:]
 }
@@ -162,65 +185,43 @@ def zwaveEvent(physicalgraph.zwave.Command cmd) {
 def on() {
     delayBetween([
         zwave.basicV1.basicSet(value: 0xFF).format(),
-        zwave.switchBinaryV1.switchBinaryGet().format()
+        zwave.basicV1.basicGet().format()
     ])
 }
 
 def both() {
-    delayBetween([
-        zwave.basicV1.basicSet(value: 0xFF).format(),
-        zwave.switchBinaryV1.switchBinaryGet().format()
-    ])
+    on()
 }
 
 def siren() {
-    delayBetween([
-        zwave.basicV1.basicSet(value: 0xFF).format(),
-        zwave.switchBinaryV1.switchBinaryGet().format()
-    ])
+    on()
 }
 
 def strobe() {
-    delayBetween([
-        zwave.basicV1.basicSet(value: 0xFF).format(),
-        zwave.switchBinaryV1.switchBinaryGet().format()
-    ])
+    on()
 }
 
 def off() {
     delayBetween([
         zwave.basicV1.basicSet(value: 0x00).format(),
-        zwave.switchBinaryV1.switchBinaryGet().format()
-    ])
-}
-
-// used by device-watch
-def ping() {
-    delayBetween([
-        zwave.switchBinaryV1.switchBinaryGet().format(),
-        zwave.manufacturerSpecificV1.manufacturerSpecificGet().format()
-    ])
-}
-
-def poll() {
-    delayBetween([
-        zwave.switchBinaryV1.switchBinaryGet().format(),
-        zwave.manufacturerSpecificV1.manufacturerSpecificGet().format()
+        zwave.basicV1.basicGet().format()
     ])
 }
 
 def refresh() {
-    delayBetween([
-        zwave.switchBinaryV1.switchBinaryGet().format(),
-        zwave.manufacturerSpecificV1.manufacturerSpecificGet().format()
-    ])
+    def commands = []
+    commands << zwave.basicV1.basicGet().format()
+    if (getDataValue("MSR") == null) {
+        commands << zwave.manufacturerSpecificV1.manufacturerSpecificGet().format()
+    }
+    delayBetween(commands)
 }
 
-def invertSwitch(invert=true) {
-    if (invert) {
-        zwave.configurationV1.configurationSet(configurationValue: [1], parameterNumber: 4, size: 1).format()
-    }
-    else {
-        zwave.configurationV1.configurationSet(configurationValue: [0], parameterNumber: 4, size: 1).format()
-    }
+// ping() is used used by Device Watch.
+def ping() {
+    refresh()
+}
+
+def poll() {
+    refresh()
 }
